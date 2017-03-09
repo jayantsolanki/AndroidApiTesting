5683307b6f817e6c6f8028e6c7368ffa92c3259f
Jayant <jayantjnp@gmail.com>
2017-03-09 15:26:38 +0530

    Two activities made

---
+++ app/src/main/java/com/example/root/apitesting/RegistrationActivity.java
@@ -0,0 +1,26 @@
+package com.example.root.apitesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
+
+        // Listening to Login Screen link
+        loginScreen.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                // Closing registration screen
+                // Switching to Login Screen/closing register screen
+                finish();
+            }
+        });
+    }
+}
